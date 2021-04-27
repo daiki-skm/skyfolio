8ba4cb116dc3c731f98dddcfc88f38f6ea83c910
佐久間大空 <daikisakuma6040@gmail.com>
2021-04-27 19:20:11 +0900

    update: 4-27

--- pages/blog.vue
+++ pages/blog.vue
@@ -1,18 +1,15 @@
 <template>
   <div class="container">
     <h1 class="sub-title">Blog</h1>
-    <!-- <div class="link-card"> -->
-      <!-- <el-card class="box-card"> -->
-        <div class="card-day">4/27/2021</div>
-        <div class="card-title">私色</div>
-        <div class="card-sentence">
-          はじめまして。佐久間です。感じたこと、思ったこと、日常など、つらつらとゆるく書いていけたらと思います。<br />
-          最近、コードを書くことが楽しくなってきました。ユーモアあふれるような、面白いものを作成していけたらと思います。<br />
-          こうして自分の気持ちや考えを書き出すことってあまりなかったため、なんだか今不思議な気持ちです。心の中が整理されていくような、そんな感覚です。<br />
-          読んでくださってありがとうございました。
-        </div>
-      <!-- </el-card> -->
-    <!-- </div> -->
+    <h3 class="blog-title">私色</h3>
+    <p class="blog-day">4/27/2021</p>
+    <p class="blog-sentence">
+      はじめまして。佐久間です。感じたこと、思ったこと、日常など、つらつらとゆるく書いていけたらと思います。<br />
+      最近、コードを書くことが楽しくなってきました。ユーモアあふれるような、面白いものを作成していけたらと思います。<br />
+      こうして自分の気持ちや考えを書き出すことってあまりなかったため、なんだか今不思議な気持ちです。心の中が整理されていくような、そんな感覚です。<br />
+      ブログの終わり方って難しいですね。どういう感じで終わりに結びつければいいかわからないです。<br />
+      拙い文章ですが読んでくださってありがとうございました。それでは。
+    </p>
     <div class="arrow-up">
       <i class="el-icon-arrow-up" @click="scrollUp" />
     </div>
@@ -63,7 +60,6 @@ export default Vue.extend({
   width: 960px;
   height: 420px;
   margin-bottom: 50px;
-  /* text-align: center; */
 }
 
 a {
@@ -78,9 +74,24 @@ a {
   transition-duration: 0.3s;
 }
 
-.card-title {
-  /* font-size: 50px;
-  line-height: 370px; */
+.blog-title {
+  float: left;
+  width: 70%;
+  text-align: left;
+  font-size: 32px;
+  margin-bottom: 50px;
+}
+
+.blog-day {
+  float: right;
+  width: 30%;
+  margin-top: 15px;
+  margin-bottom: 63px;
+  text-align: right;
+}
+
+.blog-sentence {
+  margin-bottom: 50px;
 }
 
 .arrow-up {
